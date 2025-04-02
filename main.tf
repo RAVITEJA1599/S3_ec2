4999a0d17a5b1e17abda65fb51b44481aa915394
RAVITEJA1599 <78315652+RAVITEJA1599@users.noreply.github.com>
2025-04-02 09:28:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source     = ".s3_bucket"
+  source     = "./s3_bucket"
   bucket_name = var.bucket_name
 }
 
 module "ec2_instance" {
-  source          = ".ec2_instance"
+  source          = "./ec2_instance"
   instance_type   = var.instance_type
   bucket_arn      = module.s3_bucket.bucket_arn
   key_name        = var.key_name
